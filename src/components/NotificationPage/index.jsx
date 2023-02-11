41b921920f73e16034ca22932df6d2a5508110ed
svandu <svandu08@gmail.com>
2023-02-11 23:42:01 +0530

    minor changes

--- src/components/NotificationPage/index.jsx
+++ src/components/NotificationPage/index.jsx
@@ -1,67 +1,85 @@
-import React from 'react'
+import React from "react";
 
 function NotificationPage(props) {
-
-    const notification = [
-        {
-            name: "Mark Webber",
-            para: "reacted to your recent post",
-            topic: "My first tournament today!",
-            time: "1m ago",
-        },
-        {
-            name: "Angela Gray",
-            para: "followed you",
-            time: "5m ago",
-        },
-        {
-            name: "Jacob Thompson",
-            para: "has joined your group",
-            topic: "Chess Club",
-            time: "1 day ago",
-        },
-        {
-            name: "Rizky Hasanuddin",
-            para: "sent you a private message",
-            time: "5 days ago",
-            message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game",
-        }, 
-        {
-            name: "Kimberly Smith", 
-            time: "1 week ago",
-        },
-        {
-            name: "Nathan Peterson",
-            topic: "5 end-game strategies to increase you win rate",
-            time: "2 weeks ago"
-        }, 
-        {
-            name: "Anna Kim", 
-            topic: "Chess Club", 
-            time: "2 weeks ago",
-        }
-    ]
-
+  const notification = [
+    {
+      img: "./public/assets/img/avatar-mark-webber.webp",
+      name: "Mark Webber",
+      para: "reacted to your recent post",
+      topic: "My first tournament today!",
+      time: "1m ago",
+    },
+    {
+      img: "./public/assets/img/avatar-angela-gray.webp",
+      name: "Angela Gray",
+      para: "followed you",
+      time: "5m ago",
+    },
+    {
+      img: "./public/assets/img/avatar-jacob-thompson.webp",
+      name: "Jacob Thompson",
+      para: "has joined your group",
+      topic: "Chess Club",
+      time: "1 day ago",
+    },
+    {
+      img: "./public/assets/img/avatar-rizky-hasanuddin.webp",
+      name: "Rizky Hasanuddin",
+      para: "sent you a private message",
+      time: "5 days ago",
+      message:
+        "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game",
+    },
+    {
+      img: "./public/assets/img/avatar-kimberly-smith.webp",
+      name: "Kimberly Smith",
+      para: "commented on your picture",
+      time: "1 week ago",
+      msg_img: "./public/assets/img/image-chess.webp",
+    },
+    {
+      img: "./public/assets/img/avatar-nathan-peterson.webp",
+      name: "Nathan Peterson",
+      para: "reacted to your recent post",
+      topic: "5 end-game strategies to increase you win rate",
+      time: "2 weeks ago",
+    },
+    {
+      img: "./public/assets/img/avatar-anna-kim.webp",
+      name: "Anna Kim",
+      para: "left the group",
+      topic: "Chess Club",
+      time: "2 weeks ago",
+    },
+  ];
 
   return (
-    <div className='main-container'>
-        <div className='header-container'>
-            <div className='first-part'>
-                <p className='notification-logo'>Notifications</p>
-                <button className='notification-number'>3</button>
-            </div>
-            <p className='read-all'>Mark all as read</p>
+    <div className="main-container">
+      <div className="header-container">
+        <div className="first-part">
+          <p className="notification-logo">Notifications</p>
+          <button className="notification-number">3</button>
         </div>
-        {notification.map((singleNotification, i) => (
-            <div key={i}>
-                <span className='person-name'>{singleNotification.name}</span> {singleNotification.para} <span>{singleNotification.topic} </span> {singleNotification.time}
-                <div className='private_msg'>
-                    {singleNotification.message}
-                </div>
-            </div>
-        ))}
+        <p className="read-all">Mark all as read</p>
+      </div>
+      {notification.map((singleNotification, i) => (
+        <div key={i} className="notification-msg">
+          <div className="notification-line">
+            <img
+              className="profile-img"
+              src={singleNotification.img}
+              alt="profile image"
+            />
+            <span className="person-name">{singleNotification.name}</span>{" "}
+            {singleNotification.para}{" "}
+            <span className="person-topic">{singleNotification.topic} </span>
+          </div>
+          <span className="msg-time">{singleNotification.time}</span>
+          <div className="private_msg">{singleNotification.message}</div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default NotificationPage;
